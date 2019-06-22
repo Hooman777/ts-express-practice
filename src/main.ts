97c2d7b424c4993b03f00b80ba6f381671b7541e
hooman777 <eng.hesamyan@gmail.com>
2019-06-22 09:42:30 +0400

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import * as express from 'express';
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import * as cookieParser from 'cookie-parser';
 import {Sequelize} from 'sequelize-typescript';
 import { User } from './models/user';
 import * as bodyParser from 'body-parser';
 import * as expressSession from 'express-session';
 import * as connectSessionSequelize from 'connect-session-sequelize';
+import * as path from 'path';
 
 let sequelizeSessionStore = connectSessionSequelize(expressSession.Store);
 let sequelize = new Sequelize({
@@ -27,7 +28,7 @@ const app = express();
 const port = 3000;
 app.locals.pretty = true;
 
-app.set('views', 'views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // support parsing of application/json type post data
@@ -45,17 +46,41 @@ app.use(expressSession({
     proxy: true     // if you do SSL outside of node.
 }));
 
-app.get('/', async (_: Request, res: Response) => {
-    // const person = User.build({
-    //     name: 'boo',
-    //     username: 'bar',
-    //     password: 'baz'
-    // } as User);
-    //
-    // await person.save();
+// tslint:disable-next-line:no-suspicious-comment
+// TODO: use the session to see if user is logged in, if not logged in then
+// redirect the user to login page, else redirect to /
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log(req);
+    console.log(res);
+    console.log(next);
+
+    if (!req.session.user) {
+        res.redirect("/")
+    } else {
+        next();
+    }
+});
+
+// Home page, only authorized users should be able to log-in here
+app.get('/', async (req: expressSession.Request, res: Response) => {
+/*    const person = User.build({
+        name: 'boo',
+        username: 'bar',
+        password: 'baz'
+    } as User);
+
+    await person.save();*/
+
+    req.session.user = "googz";
 
     res.render('index');
 });
+
+// TODO: return login on /login
+// TODO: handle login POST
+// TODO: return register on /register
+// TODO: handle register POST
+// TOOD: clean session on /logout GET route
 
 sequelize.sync({ force: true }).then();
 
--- src/models/user.ts
+++ src/models/user.ts
@@ -11,6 +11,11 @@ export class User extends Model<User> {
     @Column({
         type: DataType.STRING
     })
+    email: string;
+
+    @Column({
+        type: DataType.STRING
+    })
     username: string;
 
     @Column({
